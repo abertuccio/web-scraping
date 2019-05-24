4638e527af8aae4f40d7ac309a784e906fc1f1de
jlobasso <jlobasso@gmail.com>
2019-05-24 09:19:38 -0300

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const  express = require('express');
 const app = express();
 const jsonImages = require("./getJsonImages");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('tranki, anda ;)');
 });
